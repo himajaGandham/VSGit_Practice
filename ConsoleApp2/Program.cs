5a0dc0523681b088e4872586d6c043c1cb7e26e8
himajaGandham <42212517+himajaGandham@users.noreply.github.com>
2019-06-01 22:37:24 +0100

    checking sync functionality
    sync fun

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-           //Actual code starts from below line
+           //Actual code starts from below lines
         }
     }
 }
